e86460526d1dfee5605e5ffde0c78839883ed57f
janeza110519 <janezamalaya4@gmail.com>
2026-04-08 18:33:59 +0800

    LoginFrame

---
+++ src/MIDTERM/LoginSignup.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MIDTERM;
+
+/**
+ *
+ * @author Admin
+ */
+public class LoginSignup {
+    
+    
+    public static void main(String[] args) {
+        //TODO code application logic here
+         Login LoginFrame = new Login();
+         LoginFrame.setVisible(true);
+         LoginFrame.pack();
+         LoginFrame.setLocationRelativeTo(null);
+    }
+    
+}
